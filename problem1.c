51a7fd3f758000547646666a8b0fdff5be8d659a
Kaxa Gvimradze <67585860+admonterso@users.noreply.github.com>
2022-11-17 10:28:52 +0400

    Create problem1.c

---
+++ problem1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    //the formulas are from web https://www.algebra.com/algebra/homework/coordinate/Linear-systems.faq.question.1090975.html
+    
+    double A, B, C ,D,E,F, x1, x2, x,y1, y2, y;
+     scanf("%lf %lf %lf %lf %lf %lf", &A, &B, &C, &D, &E, &F ) ;
+     x1 = (C*E - F*B);
+     x2 = (A*E - B*D);
+     
+     y1 = (A*F - C*D);
+     y2 = (A*E - B*D);
+     
+     y = y1/y2;
+     
+     if (x2 == 0)
+         {
+         printf("no answer");
+         }
+     x = x1/x2;
+     
+     y = (A*F - C*D) / (A*E - B*D);
+     
+     printf("%lf %lf", x, y);
+    return 0;
+}
